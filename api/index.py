4f25bfef98dbe35d980d98e2767d97a71c16cc4e
manuel-delverme <manuel.delverme@gmail.com>
2024-10-03 17:59:22 -0700

    b

--- api/index.py
+++ api/index.py
@@ -7,7 +7,6 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     import requests
-    # https://hooks.slack.com/services/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
     requests.post("https://hooks.slack.com/services/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",
-     json={"text": "Hello FastAPI"})
-    return {"message": "Hello FastAPI"}
+     json={"text": "Hi"})
+    return {"message": "Hi"}
